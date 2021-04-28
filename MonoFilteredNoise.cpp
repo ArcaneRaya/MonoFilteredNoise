d371bc0d6122462a6313e13bb9f9431949f782bc
Ruben Bergshoeff <ruben@runicsounds.com>
2021-04-28 11:46:34 +0200

    simple mono noise generation, added filter but commented it as it does not output any sound

---
+++ MonoFilteredNoise.cpp
@@ -0,0 +1,123 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "fmod.hpp"
+#include "Iir.h"
+
+extern "C" {
+	F_EXPORT FMOD_DSP_DESCRIPTION* F_CALL FMODGetDSPDescription();
+}
+
+
+FMOD_RESULT F_CALLBACK MFN_dspCreate(FMOD_DSP_STATE* dsp);
+FMOD_RESULT F_CALLBACK MFN_dspRelease(FMOD_DSP_STATE* dsp);
+FMOD_RESULT F_CALLBACK MFN_dspReset(FMOD_DSP_STATE* dsp);
+FMOD_RESULT F_CALLBACK MFN_dspProcess(FMOD_DSP_STATE* dsp, unsigned int length, const FMOD_DSP_BUFFER_ARRAY* inBufferArray, FMOD_DSP_BUFFER_ARRAY* outBufferArray, FMOD_BOOL inputsIdle, FMOD_DSP_PROCESS_OPERATION processOperation);
+
+FMOD_DSP_DESCRIPTION FMOD_MFN_Desc =
+{
+    FMOD_PLUGIN_SDK_VERSION,                // plugin SDK version
+    "MonoFilteredNoise",                    // name (max 32char)
+    0x00010000,                             // plug-in version
+    0,                                      // number of input buffers to process
+    1,                                      // number of output buffers to process
+    MFN_dspCreate,                          // create
+    MFN_dspRelease,                         // release
+    MFN_dspReset,                           // reset
+    0,                                      // read
+    MFN_dspProcess,                         // process
+    0,                                      // set position
+    0,                                      // number of parameters
+    0,                                      // parameter descriptions
+    0,                                      // set float parameter
+    0,                                      // set int parameter
+    0,                                      // set bool parameter
+    0,                                      // set data parameter
+    0,                                      // get float parameter
+    0,                                      // get int parameter
+    0,                                      // get bool parameter
+    0,                                      // get data parameter
+    0,                                      // should i process
+    0,                                      // userdata
+    0,                                      // Register
+    0,                                      // Deregister
+    0                                       // Mix
+};
+
+extern "C"
+{
+    F_EXPORT FMOD_DSP_DESCRIPTION* F_CALL FMODGetDSPDescription()
+    {
+        return &FMOD_MFN_Desc;
+    }
+}
+
+class MonoFilteredNoiseState
+{
+public:
+    MonoFilteredNoiseState();
+    void generate(float* outBuffer, unsigned int length);
+private:
+    Iir::Butterworth::LowPass<8> filter;
+    const float samplingrate = 48000; // Hz
+    const float cutoff_frequency = 23999; // Hz
+    float generatedValue = 0;
+};
+
+MonoFilteredNoiseState::MonoFilteredNoiseState()
+{
+    filter.setup(samplingrate, cutoff_frequency);
+}
+
+void MonoFilteredNoiseState::generate(float* outBuffer, unsigned int length)
+{
+    while (length--) 
+    {
+        generatedValue = (((float)(rand() % 32768) / 16384.0f) - 1.0f);
+        *outBuffer++ = generatedValue; // <- works, nice white noise || filter.filter(generatedValue); <- filtered doesn't work, no sound
+    }
+}
+
+FMOD_RESULT F_CALLBACK MFN_dspCreate(FMOD_DSP_STATE* dsp) 
+{
+    dsp->plugindata = (MonoFilteredNoiseState*)FMOD_DSP_ALLOC(dsp, sizeof(MonoFilteredNoiseState));
+    if (!dsp->plugindata)
+    {
+        return FMOD_ERR_MEMORY;
+    }
+
+    return FMOD_OK;
+}
+
+FMOD_RESULT F_CALLBACK MFN_dspRelease(FMOD_DSP_STATE* dsp)
+{
+    MonoFilteredNoiseState* state = (MonoFilteredNoiseState*)dsp->plugindata;
+    FMOD_DSP_FREE(dsp, state);
+    return FMOD_OK;
+}
+
+FMOD_RESULT F_CALLBACK MFN_dspProcess(FMOD_DSP_STATE* dsp, unsigned int length, const FMOD_DSP_BUFFER_ARRAY* /*inbufferarray*/, FMOD_DSP_BUFFER_ARRAY* outBufferArray, FMOD_BOOL /*inputsidle*/, FMOD_DSP_PROCESS_OPERATION op)
+{
+    MonoFilteredNoiseState* state = (MonoFilteredNoiseState*)dsp->plugindata;
+
+    if (op == FMOD_DSP_PROCESS_QUERY)
+    {
+        if (outBufferArray) 
+        {
+            outBufferArray->speakermode = FMOD_SPEAKERMODE_MONO;
+            outBufferArray->buffernumchannels[0] = 1;
+        }
+
+        return FMOD_OK;
+    }
+
+    state->generate(outBufferArray->buffers[0], length);
+    return FMOD_OK;
+}
+
+FMOD_RESULT F_CALLBACK MFN_dspReset(FMOD_DSP_STATE* dsp) 
+{
+    return FMOD_OK;
+}
